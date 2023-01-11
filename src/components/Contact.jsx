6b5b8e6486669dac84c2f3bbd3ebf8adad673518
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-11 13:18:34 +0700

    responsive Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,17 +5,20 @@ import Button from "./Button";
 export default function Contact() {
   return (
     <section className="contact section">
-      <div className="contact__container container grid">
-        <div className="contact__data justify-self-center text-center">
+      <div className="contact__container container grid sm:grid-cols-2 sm:items-center lg:max-w-4xl">
+        <div className="contact__data justify-self-center text-center sm:text-left">
           <p className="contact__subtitle section-subtitle">Let's Talk</p>
           <h1 className="contact__title section-title">Contact Us</h1>
-          <p className="contact__text section-text">
+          <p className="contact__text section-text md:max-w-md">
             If you want to reserve a table in our restaurant, contact us and we
             will attemd you quickly, with our 24/7 chat service.
           </p>
         </div>
 
-        <Button url="/contact-us" className="justify-self-center">
+        <Button
+          url="/contact-us"
+          className="justify-self-center sm:justify-self-end"
+        >
           Contact us now
         </Button>
       </div>
