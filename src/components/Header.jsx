206e7dbf07d78cff361876481c180034d9ff3978
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-11 12:47:35 +0700

    responsive Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,11 +22,11 @@ export default function Header() {
         </Link>
 
         <div
-          className={`header__menu fixed top-24 left-0 w-full origin-top bg-gray-50 py-8 px-16 text-center shadow-lg transition-all duration-300 ${
+          className={`header__menu fixed top-24 left-0 w-full origin-top bg-gray-50 py-8 px-16 text-center shadow-lg transition-all duration-300 md:static md:top-0 md:flex md:w-auto md:scale-y-100 md:items-center md:gap-12 md:bg-transparent md:p-0 md:opacity-100 md:shadow-none ${
             menuOpen ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"
           }`}
         >
-          <ul className="header__list mb-6 flex flex-col gap-6">
+          <ul className="header__list flex flex-col gap-6 md:flex-row md:items-center">
             {[
               ["About", "/about"],
               ["Services", "/services"],
@@ -45,13 +45,13 @@ export default function Header() {
             ))}
           </ul>
 
-          <Button url="/order" className="mt-6">
+          <Button url="/order" className="mt-6 md:mt-0">
             Order Now
           </Button>
         </div>
 
         <div
-          className="header__toggle inline-flex cursor-pointer text-[1.3rem] text-gray-900"
+          className="header__toggle inline-flex cursor-pointer text-[1.3rem] text-gray-900 md:hidden"
           onClick={setMenuHandler}
         >
           {!menuOpen ? <RiMenu2Line /> : <RiCloseLine />}
