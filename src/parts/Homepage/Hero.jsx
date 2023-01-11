2741b2006e20ff5dc587836aeab973ba4f4a6754
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-11 12:44:14 +0700

    responsive Hero.jsx

--- src/parts/Homepage/Hero.jsx
+++ src/parts/Homepage/Hero.jsx
@@ -6,12 +6,12 @@ import Button from "../../components/Button";
 export default function Hero() {
   return (
     <section className="hero section">
-      <div className="hero__container container grid gap-12">
+      <div className="hero__container container grid gap-12 sm:grid-cols-2 sm:items-center lg:max-w-4xl">
         <div className="hero__data">
-          <h1 className="hero__title section-title text-[42px] text-green-600">
+          <h1 className="hero__title section-title text-[42px] text-green-600 md:text-[56px] lg:text-[64px]">
             Tasty Food
           </h1>
-          <p className="hero__text section-text text-[24px] font-bold text-gray-900">
+          <p className="hero__text section-text text-[24px] font-bold text-gray-900 md:text-[32px]">
             Try the best food <br /> of the week.
           </p>
           <Button className="inline-flex">View Menu</Button>
