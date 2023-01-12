7698930912d241d7b1b847a937774ffc81f70d57
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-12 12:57:01 +0700

    modified ScrollButton.jsx

--- src/components/ScrollButton.jsx
+++ src/components/ScrollButton.jsx
@@ -7,9 +7,9 @@ export default function ScrollButton() {
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
 
-    if (scrolled > 300) {
+    if (scrolled > 400) {
       setVisible(true);
-    } else if (scrolled <= 300) {
+    } else if (scrolled <= 400) {
       setVisible(false);
     }
   };
